58430dea6ac79f83e669d2ec557e71563f22b1b9
Romain Beaubois <rbeaubois@u-bordeaux.fr>
2022-06-28 18:28:26 +0900

    Quick script clean

--- matlab/functions/2_Processing/Filtering/filter_signal.m
+++ matlab/functions/2_Processing/Filtering/filter_signal.m
@@ -19,10 +19,10 @@ function [LP_Signal_fix, HP_Signal_fix]=filter_signal(Fs, num_electrode, t, Sign
 
     parfor i=1:num_electrode
         baseline                = mean(Signal(:,i));
-        Signal_fix(:, i)        = Signal(:, i) -baseline;
-        LP_Signal_fix(:, i)     = lowpass(Signal_fix(:, i),1000, Fs);
-        HPt_Signal_fix(:, i)    = lowpass(Signal_fix(:, i),3000, Fs);
-        HP_Signal_fix(:, i)     = highpass(HPt_Signal_fix(:, i),300, Fs);
+        Signal_fix(:, i)        = Signal(:, i) - baseline;
+        LP_Signal_fix(:, i)     = lowpass(Signal_fix(:, i), 1000, Fs);
+        HPt_Signal_fix(:, i)    = lowpass(Signal_fix(:, i), 3000, Fs);
+        HP_Signal_fix(:, i)     = highpass(HPt_Signal_fix(:, i), 300, Fs);
     end
 
     clearvars HPt_Signal_fix Signal_fix;
